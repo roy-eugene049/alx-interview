924231345fe4f36fcad118b399ce957187b6650e
roy-eugene049 <roy.eugenes000.com>
2023-04-04 17:55:31 +0300

    Task Correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,29 @@
-import sys
+#!/usr/bin/python3
+
+"""
+    Method that determines the number of minmum operations given n characters
+"""
+
 
 def minOperations(n):
-    dp = [sys.maxsize] * (n+1)
-    dp[1] = 0
-    
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i//j - 1))
-    
-    return dp[n] if dp[n] != sys.maxsize else 0
+    """
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
+    """
+
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return 
