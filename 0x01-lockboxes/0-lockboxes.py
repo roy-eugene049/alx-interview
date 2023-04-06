e9c7db7e90ad9e7d6bb2c801146b428f8379702a
roy-eugene049 <roy.eugenes000.com>
2023-04-06 16:59:03 +0300

    Latest Attempt

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Script will unlock list of lists"""
-
-
-def canUnlockAll(boxes):
-    """This function will take a list of lists and the content
-       of a list will unlock other lists
+"""Calculate the fewest number of operations needed to get n H's in a file"""
+def minOperations(n):
     """
+    Calculate the fewest number of operations needed to get n H's in a file.
 
-    keys = [0]
-    for key in keys:
-        for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
-                keys.append(boxKey)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    Args:
+        n (int): The number of H's to achieve.
+
+    Returns:
+        int: The fewest number of operations needed to get n H's, or 0 if impossible.
+    """
+    if n < 1:
+        return 0  # impossible to achieve
+    num_h = 1  # start with one H
+    ops = 0  # number of operations performed
+    clipboard = 0  # number of H characters in clipboard
+    while num_h < n:
+        if clipboard > 0:
+            # if there are H characters in clipboard, paste them
+            num_h += clipboard
+            clipboard = 0
+            ops += 1
+        else:
+            # if clipboard is empty, copy all H characters
+            clipboard = num_h
+            ops += 1
+        if num_h == n:
+            # if desired number of H characters is reached, return number of operations
+            return ops
+    return 0  # impossible to achieve
